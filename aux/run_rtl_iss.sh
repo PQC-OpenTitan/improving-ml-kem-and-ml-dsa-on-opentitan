fea6f36e7ab24050f859db81b309fcac0983157f
Pham Hoang Nguyen Hien <nguyenhien.phamhoang@gmail.com>
2025-10-28 15:37:14 +0100

    [aux] Add script for running all RTL-ISS tests

---
+++ aux/run_rtl_iss.sh
@@ -0,0 +1,19 @@
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 1
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 1 -m brent_kung -a brent_kung
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 1 -m sklansky -a sklansky
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 1 -m kogge_stone -a kogge_stone
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 1 -m csa_carry4 -a csa_carry4
+
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 2
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 2 -m brent_kung -a brent_kung
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 2 -m sklansky -a sklansky
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 2 -m kogge_stone -a kogge_stone
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 2 -m csa_carry4 -a csa_carry4
+
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 3
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 3 -m brent_kung -a brent_kung
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 3 -m sklansky -a sklansky
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 3 -m kogge_stone -a kogge_stone
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 3 -m csa_carry4 -a csa_carry4
+
+hw/ip/otbn/dv/smoke/run_rtl_iss_test.py -ver 0
